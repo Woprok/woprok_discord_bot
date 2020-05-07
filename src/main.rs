feb9001b3ea2e6ec76f5fbbf62706510f6b614f5
Miroslav Valach <skvalach@gmail.com>
2020-05-08 00:15:24 +0200

    Johny message

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,9 @@ fn main()
 {
     println!("I am Rust Bot. Smart and Edgy!");
     println!("I am Rust Bot. Smart and Edgy!");
+    slay_johnny_announcer();
+}
+
+fn slay_johnny_announcer(){
+    println!("I have come to slay Johnny Pythagoras and claim his place!");
 }
