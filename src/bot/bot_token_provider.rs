1825fa7d15a30772024fdf2699f21a3ccbb6a781
Miroslav Valach <skvalach@gmail.com>
2020-05-09 00:19:54 +0200

    next will fix constants

--- src/bot/bot_token_provider.rs
+++ src/bot/bot_token_provider.rs
@@ -1,7 +1,5 @@
 use std::env;
 
-random
-
 pub fn set_environment_variables()
 {
     env::set_var(DISCORD_TOKEN_KEY, DISCORD_TOKEN_VALUE);
--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -1,3 +1,4 @@
 pub mod example_bot_1;
 pub mod example_bot_1_1;
 pub mod bot_token_provider;
+pub mod bot_token_constants;
